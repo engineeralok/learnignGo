130dd922b6e31ac78bd9f4fd75e1923196bc7bf0
engineeralok <engineeralok07@gmail.com>
2024-05-06 22:46:35 +0530

    Day8 Learning: For loop

---
+++ Day8/simpleFor.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Println("Countdown Enter n: ")
+	fmt.Scanf("%d", &n)
+
+	for i := n; i >= 0; i-- {
+		if i == 0 {
+			fmt.Println("Blast off!")
+		} else {
+			fmt.Println("Countdown ", i)
+		}
+	}
+}
